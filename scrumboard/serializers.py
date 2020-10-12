aff8189ef9567e743399b55e338d33396c9aef52
Robert Wegwart <robert.wegwart@gmail.com>
2020-10-12 23:51:19 +0200

    refacturing using static files

--- scrumboard/serializers.py
+++ scrumboard/serializers.py
@@ -2,17 +2,16 @@ from rest_framework import serializers
 
 from .models import List, Card
 
-class ListSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = List
-        fields = ['id', 'name']
-
-
 class CardSerializer(serializers.ModelSerializer):
 
     class Meta:
          model = Card
          fields = ['id', 'title', 'description', 'story_points', 'business_value', 'list' ] 
          
+class ListSerializer(serializers.ModelSerializer):
 
+    cards = CardSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = List
+        fields = ['id', 'name', 'cards']
