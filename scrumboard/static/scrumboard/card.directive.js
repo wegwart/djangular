48fa28506fcf3d1c395b7cb6c4516e07a682deae
Robert Wegwart <robert.wegwart@gmail.com>
2020-10-24 18:51:58 +0200

    deleting cards

--- scrumboard/static/scrumboard/card.directive.js
+++ scrumboard/static/scrumboard/card.directive.js
@@ -16,6 +16,17 @@
                         $scope.card
                     );
                 };
+                $scope.delete = function () {
+                    $http.delete(url).then(
+                        function() {
+                            var cards = $scope.list.cards;
+                            cards.splice(
+                                cards.indexOf($scope.card),
+                                1
+                            );
+                        }
+                    );
+                };
                 $scope.modelOptions = {
                     debounce: 500
                 };
